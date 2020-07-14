b1eb54389c3998572034452827ae92532881a868
createfu <hw2102@outlook.com>
2020-07-14 14:43:05 +0800

    update

--- configs/config.go
+++ configs/config.go
@@ -94,9 +94,9 @@ func init() {
 	flag.StringVar(&logfile, "logfile", "", "set log file")
 	flag.StringVar(&sqlfile, "sqllog", "", "set sql log file")
 
-	//confPath = "./configs/datasources-" + env + ".toml"
+	confPath = "./configs/datasources-" + env + ".toml"
 	//test
-	confPath = "../../configs/datasources-" + env + ".toml"
+	//confPath = "../../configs/datasources-" + env + ".toml"
 
 	_, err := toml.DecodeFile(confPath, &EnvConfig)
 	if err != nil {
